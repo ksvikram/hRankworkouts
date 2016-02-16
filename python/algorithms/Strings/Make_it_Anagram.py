70e7c7668c31e7fd902a3a544fe1d35f718ff7c4
ksvikram <ksvikram04@gmail.com>
2016-02-16 15:21:30 +0530

    Update Make_it_Anagram.py

--- python/algorithms/Strings/Make_it_Anagram.py
+++ python/algorithms/Strings/Make_it_Anagram.py
@@ -3,3 +3,24 @@ a=Counter(raw_input())
 b=Counter(raw_input())
 c=a-b;d=b-a;e=c+d
 print len(list(e.elements()))
+
+'''
+#slower 
+s1 = raw_input().strip()
+s2 = raw_input().strip()
+
+if len(s1) > len(s2):
+    s1, s2 = s2, s1
+for i in s1:
+    if i in s2:
+        l1 = list(s1)
+        l2 = list(s2)
+        del(l1[s1.find(i)])
+        del(l2[s2.find(i)])
+        s1 = ''.join(l1)
+        s2 = ''.join(l2)
+  
+print len(s1) + len(s2)
+
+
+'''
