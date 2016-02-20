3ef7f1a0da79eff7e48c16f19e44ad6868163ec5
ksvikram <ksvikram04@gmail.com>
2016-02-20 17:07:10 +0530

    Create valid_bst.py

---
+++ python/functional_programming/functional_structures/valid_bst.py
@@ -0,0 +1,22 @@
+def isBST(a):
+    stack = []
+	lower = -1
+	state = True
+	for item in a:
+		if lower > -1 and item < lower:
+			state = False
+		while(len(stack) != 0 and stack[len(stack) - 1]  < item ):
+			lower = stack.pop()
+		stack.append(item)
+
+	if state == False:
+		print "NO"
+	else:
+		print "YES"
+
+t = int(raw_input().strip())
+
+for _ in range(t):
+    n = int(raw_input().strip())
+    l = map(int, raw_input().strip().split())
+    isBST(l)
