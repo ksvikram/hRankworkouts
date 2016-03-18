025da8c22f9d1162a6997d9816d2105f28f591fe
ksvikram <ksvikram04@gmail.com>
2016-03-18 11:47:57 +0530

    Create Sansa_and_XOR.py

---
+++ python/algorithms/Bit-Manipulation/Sansa_and_XOR.py
@@ -0,0 +1,10 @@
+#!/bin/python
+for _ in range(input()):
+    res = 0
+    n = input()
+    ar = map(int, raw_input().strip().split())
+    # if array is of odd length, xor all items in odd position
+    if len(ar)%2 != 0 :
+        for i in range(0, len(ar)+1/2,2):
+            res ^= ar[i]
+    print res    
